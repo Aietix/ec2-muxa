5638fe9e7e46d81a11f69e6e7599ab73beee35fc
Giorgi <giorgi.tcholadze@gmail.com>
2023-06-14 15:02:25 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "instance_id" {
   value       = aws_instance.this.id
 }
 
-output "IP_address" {
+output "ip_address" {
   description = "Elastic IP of the EC2 instance if assigned, otherwise Public IP."
   value       = length(aws_eip.this) > 0 ? aws_eip.this[0].public_ip : aws_instance.this.public_ip
 }
 
-output "used_linux_image" {
+output "linux_image" {
   description = "Type of Linux AMI used (Amazon or Ubuntu)."
   value       = var.ami_type == "amazon" ? "Amazon Linux AMI used" : "Ubuntu Linux AMI used"
 }
