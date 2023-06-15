7b7790b86fa44fd1619233e2ef3831d02ebb4b3c
Giorgi <giorgi.tcholadze@gmail.com>
2023-06-16 01:30:24 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,16 @@ provider "aws" {
 
 # Define an EC2 instance
 resource "aws_instance" "this" {
-  ami                         = var.ami_type == "amazon" ? data.aws_ami.amazon[0].id : data.aws_ami.ubuntu[0].id
+  ami                         = local.ami_type == "amazon" ? data.aws_ami.amazon[0].id : data.aws_ami.ubuntu[0].id
   instance_type               = var.instance_type
   vpc_security_group_ids      = [aws_security_group.this.id]
   key_name                    = var.use_key_pair ? aws_key_pair.this[0].key_name : null
   user_data                   = fileexists("user_data.sh") ? file("user_data.sh") : null
   subnet_id                   = local.subnet_id
-  associate_public_ip_address = true
+  associate_public_ip_address = var.public_ip
 
   tags = {
-    Name = var.tags
+    Name = var.name
   }
 }
 
