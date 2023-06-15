980f6f755484b173c08bc6e75b72064b4e239be7
Giorgi <giorgi.tcholadze@gmail.com>
2023-06-15 15:30:08 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "this" {
   vpc_security_group_ids      = [aws_security_group.this.id]
   key_name                    = var.use_key_pair ? aws_key_pair.this[0].key_name : null
   user_data                   = fileexists("user_data.sh") ? file("user_data.sh") : null
-  subnet_id                   = var.subnet_id != "" ? var.subnet_id : tolist(data.aws_subnets.default.ids)[0]
+  subnet_id                   = local.subnet_id
   associate_public_ip_address = true
 
   tags = {
